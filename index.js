b5576687b2cf1efcc52e4bc87b6053cbb58a8eac
wigsters52 <ryanwiggins52@gmail.com>
2021-02-04 19:47:56 -0500

    added movement

--- index.js
+++ index.js
@@ -52,3 +52,34 @@ function createBoard() {
 }
 
 createBoard()
+
+
+// starting position of pacman
+
+let pacmanCurrentIndex = 490
+squares[pacmanCurrentIndex].classList.add('pacman')
+
+function control(e) {
+    squares[pacmanCurrentIndex].classList.remove('pacman')
+    switch (e.keyCode) {
+        case 40:
+            console.log('pressed down')
+            if (pacmanCurrentIndex + width < width * width) pacmanCurrentIndex += width
+            break
+        case 38:
+            console.log('pressed up')
+            if (pacmanCurrentIndex - width >= 0) pacmanCurrentIndex -= width
+            break
+        case 37:
+            console.log('pressed left')
+            if (pacmanCurrentIndex % width !== 0) pacmanCurrentIndex -= 1
+            break
+        case 39:
+            console.log('pressed right')
+            if (pacmanCurrentIndex % width < width - 1) pacmanCurrentIndex += 1
+
+            break
+    }
+    squares[pacmanCurrentIndex].classList.add('pacman')
+}
+document.addEventListener('keyup', control)
